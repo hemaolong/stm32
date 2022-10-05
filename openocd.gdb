1accc8581625c6ada0bf6fe3b53ad7a108ee4ce0
hemaolong <maolong.he@gmail.com>
2022-10-05 13:34:05 +0800

    增加多种点亮方案

--- openocd.gdb
+++ openocd.gdb
@@ -21,7 +21,7 @@ monitor tpiu config internal itm.txt uart off 8000000
 monitor itm port 0 on
 
 # Set a breakpoint at main, aka entry
-break main
+# break main
 
 # Set a breakpoint at DefaultHandler
 break DefaultHandler
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-#![deny(unsafe_code)]
+// #![deny(unsafe_code)]
 #![no_main]
 #![no_std]
-
 // use aux5::entry;
 
 // 安装Cargo bin工具 cargo install cargo-binutils
@@ -28,20 +27,59 @@
 //! Note: Without additional hardware, PC13 should not be used to drive an LED, see page 5.1.2 of
 //! the reference manual for an explanation. This is not an issue on the blue pill.
 
-
 use panic_halt as _;
+
+use core::ptr;
 
 use nb::block;
 
 use cortex_m_rt::entry;
 use stm32f1xx_hal::{pac, prelude::*, timer::Timer};
 
+// 引入println等
+pub use cortex_m::{asm::bkpt, iprint, iprintln, peripheral::ITM};
+pub use stm32f1xx_hal::stm32::{self, gpioa::RegisterBlock};
+
+// use cortex_m_semihosting::hprintln;
+
+// 通过gpioc-brr操作led灯
+fn blink_by_brr(v:bool){
+  let gpioc = unsafe { &*stm32::GPIOC::ptr() };
+  if v {
+    gpioc.bsrr.write(|w| w.bs13().set_bit());
+  }else{
+    gpioc.bsrr.write(|w| w.br13().set_bit());
+  }
+
+  // cortex_m::asm::bkpt();
+}
+
+// 通过寄存器地址操作led灯
+// GPIO对应一个内存偏移地址：0x4001_1000，GPIOC（stm32f103LED灯对应pc13）偏移：0x10
+fn blink_by_addr(v:bool){
+  unsafe{
+    // Memory map and register boundary addresses. Page 51,
+    // Offset. Page 168
+    // pc13 寄存器地址
+    const GPIOC_BSRR: u32 = 0x4001_1000 + 0x10; // 寄存器地址+BSRR偏移地址
+
+    // Trun off all LED
+    let offset = if v { 1 << 13}  else {1 << 13 + 16};
+    ptr::write_volatile(GPIOC_BSRR as *mut u32, offset);
+  }
+}
+
+// 通过rcc处理RST开关
+fn blink_by_moder_and_rcc(){
+  let rcc = unsafe { &*stm32::RCC::ptr() };
+  rcc.ahbenr.write(|w| w.crcen().set_bit());
+}
 
 // heml
 #[entry]
 fn main() -> ! {
     // Get access to the core peripherals from the cortex-m crate
-    let cp = cortex_m::Peripherals::take().unwrap();
+    let mut cp = cortex_m::Peripherals::take().unwrap();
     // Get access to the device specific peripherals from the peripheral access crate
     let dp = pac::Peripherals::take().unwrap();
 
@@ -58,27 +96,40 @@ fn main() -> ! {
 
     // // Configure gpio C pin 13 as a push-pull output. The `crh` register is passed to the function
     // // in order to configure the port. For pins 0-7, crl should be passed instead.
-    
+
+
     let mut gpioc = dp.GPIOC.split();
     let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
 
-  //   let mut gpiob = dp.GPIOB.split();
-  // let mut led = gpiob.pb12.into_push_pull_output(&mut gpiob.crh);
+    // blink_by_moder_and_rcc();
+    //   let mut gpiob = dp.GPIOB.split();
+    // let mut led = gpiob.pb12.into_push_pull_output(&mut gpiob.crh);
 
     // let mut gpiod = dp.GPIOD.split();
     // let mut led = gpiod.pd2.into_push_pull_output(&mut gpiod.crl);
 
-
+    iprintln!(&mut (cp.ITM.stim[0]), "hello world");
+    // iprintln!(&mut (cp.ITM.stim[1]),"hello world");
+    // hprintln!("Hello, world!").unwrap();
 
     // Configure the syst timer to trigger an update every second
     let mut timer = Timer::syst(cp.SYST, &clocks).counter_hz();
     timer.start(1.Hz()).unwrap();
 
     // Wait for the timer to trigger an update and change the state of the LED
+    // gpioe.bsrr.write(|w| w.br13().set_bit());
+    // gpioe.bsrr.write(|w| w.bs13().set_bit());
+
+    led.set_high();
+  
     loop {
         block!(timer.wait()).unwrap();
-        led.set_high();
+
+        // led.set_high();
+        blink_by_brr(true);
         block!(timer.wait()).unwrap();
-        led.set_low();
+        // led.set_low();
+        blink_by_brr(false);
+
     }
 }
